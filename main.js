193c634ba385845b43c1667356bcb453b68936db
Ferbus <ferver96@gmail.com>
2023-05-05 23:16:58 -0700

    card default layout and card not found layout added

--- main.js
+++ main.js
@@ -25,25 +25,46 @@ function getMovie (){
                 for (genre of genresArray){
                     genreBadge += `<h3 class="col-auto badge bg-dark rounded-pill me-1"> ${genre} </h3>`;
                 }
-                console.log(genreBadge)
                 return genreBadge
             }
 
+            let runtime = ""
+            data.Type == "movie" ? runtime = ` (${data.Runtime})` : runtime
+
             cardBody.innerHTML = `
                 <h5 class="card-title mb-1">${data.Title}</h5>
-                <h6 class="card-subtitle text-muted mb-2">${data.Type} | ${data.Year}</h6>
+                <h6 class="card-subtitle text-muted mb-2">${data.Type}${runtime} | ${data.Year}</h6>
                 <span class='card-subtitle text-start mb-2 col-12'>
                     <div class="row g-0">
                         ${getGenres(genres)}
                     </div>
                 </span>
                 <img class="card-img mb-2" src="${data.Poster}" alt="">
-                <p class="card-text">${data.Plot}</p>
+                <div class="row g-0 mb-1">
+                    <p class="col-12 card-text fw-bold mb-0">Plot</p>
+                    <p class="card-text"> ${data.Plot}</p>
+                </div>
+                <div class="row g-0 justify-content-center align-items-center text-center fw-bold border-top border-1 border-warning mt-3 p-2">
+                    <p class="col-12 card-text mb-0">imdb Rating</p>
+                    <img src="images/star.svg" class="col-1"  width="20" height="20">
+                    <p class="card-text text-warning col-auto"> ${data.imdbRating}</p>
+                </div>
             `
     
         }
         else{
-            cardBody.innerHTML = "<h2>Movie not found</h2>"
+
+            if(userInput.value.trim() != ""){
+            cardBody.innerHTML = `
+            <div class="text-center">
+            <h2 class="card-title mb-2">Movie not found!</h2>
+            <h6 class="card-subtitle text-muted">Mayba you wrote "<b> ${userInput.value} </b>" wrong.</h6>
+            </div>
+            `
+            }
+            else {
+                userInput.setAttribute('placeholder', 'Type something to search');
+            }
         }
     })
 }
